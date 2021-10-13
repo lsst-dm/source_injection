06ea9b05421c4648cf30265530797dd6226aa5aa
Josh Meyers <jmeyers314@gmail.com>
2021-10-13 12:38:10 -0700

    Update doc/conf.py for new template

--- doc/conf.py
+++ doc/conf.py
@@ -1,13 +1,14 @@
 """Sphinx configuration file for an LSST stack package.
 
 This configuration only affects single-package Sphinx documentation builds.
+For more information, see:
+https://developer.lsst.io/stack/building-single-package-docs.html
 """
 
-from documenteer.sphinxconfig.stackconf import build_package_configs
-import lsst.source_injection
+from documenteer.conf.pipelinespkg import *
 
 
-_g = globals()
-_g.update(build_package_configs(
-    project_name='source_injection',
-    version=lsst.source_injection.version.__version__))
+project = "source_injection"
+html_theme_options["logotext"] = project
+html_title = project
+html_short_title = project
